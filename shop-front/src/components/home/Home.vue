99a77d4075ed01cd5b7929baa8421890f9cb64ad
hyfly233 <1274694743@qq.com>
2022-03-26 11:31:13 +0800

    commit

---
+++ shop-front/src/components/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home">
+    <div id="text">{{text}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import home from './home.components'
+
+export default {
+  ...home
+}
+</script>
+
+<style lang="less" scoped>
+@import "./home.components";
+</style>
