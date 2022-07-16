c45f8ee18c2c4ffc1020696b736e77b4722d0ccd
hyfly233 <1274694743@qq.com>
2022-07-16 13:24:20 +0800

    commit

---
+++ order/src/main/java/com/hyfly/milet/shop/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.hyfly.milet.shop.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
